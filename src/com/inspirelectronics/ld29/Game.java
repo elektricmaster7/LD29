64f49925604fb7ca33e061edd4b7726b90df0f25
João Redondo <geral@inspirelectronics.com>
2014-04-26 04:53:40 +0100

    Functional Graphics Implementation

--- src/com/inspirelectronics/ld29/Game.java
+++ src/com/inspirelectronics/ld29/Game.java
@@ -1,33 +1,76 @@
 package com.inspirelectronics.ld29;
 
+import com.inspirelectronics.ld29.graphics.Color;
+import com.inspirelectronics.ld29.graphics.Display;
+import com.inspirelectronics.ld29.graphics.Screen;
 import com.inspirelectronics.ld29.graphics.Window;
 
 public class Game implements Runnable{
 	
-	public static final int WIDTH = 240;
-	public static final int HEIGHT = 160;
+	public static final int WIDTH = 1280/2;
+	public static final int HEIGHT = 720/2;
+	public static final float SCALE = 2.0f;
 	public static final String TITLE = "LD29";
 	
+	//Logic
 	private boolean running;
 	private Thread thread;
-	private Window window;
+	
+	//Graphics
+	public int framerate = 60;
+	public Screen screen;
+	private Display display;
 	
 	public void init(){
 		//Setup Window
-		window = new Window("LD29", 260, 160);
+		display = new Display(new Window(TITLE, WIDTH, HEIGHT), SCALE);
+		screen = new Screen(WIDTH, HEIGHT, SCALE);
 	}
 	
 	public void run() {
 		init();
-		System.out.println("ADAD");
+		long lt = System.nanoTime();
+		double dt = 0;
+		double ns = 1000000000.0 / framerate;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		int updates = 0;
+		while(running){
+			long now = System.nanoTime();
+			dt += (now - lt) / ns;
+			lt = now;
+			if(dt >= 1){
+				update();
+				updates++;
+				dt--;
+			}
+			render();
+			frames++;
+			if(System.currentTimeMillis() - timer > 1000){
+				timer += 1000;
+				System.out.println(frames+"fps, "+updates+"ups");
+				updates = 0;
+				frames = 0;
+			}
+		}
 	}
 	
 	public void render(){
+		screen.clear();
 		
+		screen.fillRect(0, 1, 10, 10, Color.GREEN);
+		
+		show();
 	}
 	
 	public void update(){
 		
+	}
+	
+	public void show(){
+		screen.flip();
+		display.draw(screen.getImage());
+		display.show();
 	}
 	
 	public synchronized void start(){
--- src/com/inspirelectronics/ld29/graphics/Screen.java
+++ src/com/inspirelectronics/ld29/graphics/Screen.java
@@ -1,5 +1,52 @@
 package com.inspirelectronics.ld29.graphics;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
 public class Screen {
+
+	public int width, height;
+	public float scale;
+	public int[] buffer, image_pixels;
+	
+	private BufferedImage image;
+	
+	public Screen(int width, int height, float scale) {
+		this.width = (int) (width / scale);
+		this.height = (int) (height / scale);
+		this.scale = scale;
+		buffer = new int[this.width * this.height];
+		image = new BufferedImage(this.width, this.height, BufferedImage.TYPE_INT_RGB);
+		image_pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+	}
+	
+	public void flip(){
+		for(int i = 0; i < width * height; i++){
+			image_pixels[i] = buffer[i];
+		}
+	}
+	
+	public void clear(Color color){
+		for(int i = 0; i < width * height; i++){
+			buffer[i] = color.getColor();
+		}
+	}
+	
+	public void clear(){
+		clear(Color.BLACK);
+	}
+	
+	public void fillRect(int x, int y, int width, int height, Color color) {
+		for (int yy = y; yy < y + height; yy++) {
+			for (int xx = x; xx < x + width; xx++) {
+				if (xx < 0 || xx >= this.width || yy < 0 || yy >= this.height) continue;
+				buffer[xx + yy * this.width] = color.getColor();
+			}
+		}
+	}
+	
+	public BufferedImage getImage(){
+		return image;
+	}
 
 }
