d6529c58cf5cd798b75dcc15db1bfdeecdfb0ed3
João Redondo <geral@inspirelectronics.com>
2014-04-26 07:58:16 +0100

    Added Sprite and SpriteSheet Functionality

---
+++ src/com/inspirelectronics/ld29/graphics/Sprite.java
@@ -0,0 +1,20 @@
+package com.inspirelectronics.ld29.graphics;
+
+public class Sprite {
+	
+	public Bitmap bitmap;
+	public int offsetX = 0, offsetY = 0;
+	
+	public Sprite(){
+		
+	}
+	
+	public Sprite(Bitmap bitmap){
+		this.bitmap = bitmap;
+	}
+	
+	public void render(int x, int y, Screen screen){
+		screen.render(bitmap, x+offsetX, y+offsetY);
+	}
+	
+}
---
+++ src/com/inspirelectronics/ld29/graphics/SpriteSheet.java
@@ -0,0 +1,28 @@
+package com.inspirelectronics.ld29.graphics;
+
+public class SpriteSheet {
+	
+	public Bitmap bitmap;
+	public int sw, sh;
+	
+	public SpriteSheet(Bitmap bitmap, int sw, int sh){
+		this.bitmap = bitmap;
+		this.sw = sw;
+		this.sh = sh;
+	}
+	
+	public Sprite getSprite(int sx, int sy){
+		Bitmap buffer = new Bitmap(sw, sh); 
+		int y = bitmap.getHeight();
+		int x = bitmap.getWidth();
+		for (int yy = 0; yy < sh; yy++) {
+			int yp = yy + y;
+			for(int xx = 0; xx < sw; xx++){
+				int xp = xx + x;
+				if (xp < 0 || xp >= bitmap.getWidth() || yp < 0 || yp >= bitmap.getHeight()) continue;
+				buffer.pixels[xx + yy * buffer.getWidth()] = bitmap.pixels[xp + yp * bitmap.getWidth()];
+			}
+		}
+		return new Sprite(buffer);
+	}
+}
