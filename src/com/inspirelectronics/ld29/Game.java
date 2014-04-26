1a4347c04a4d1505aa1d10dae305fc0f1af08c5c
João Redondo <geral@inspirelectronics.com>
2014-04-26 03:00:58 +0100

    Added Window Class

--- src/com/inspirelectronics/ld29/Game.java
+++ src/com/inspirelectronics/ld29/Game.java
@@ -1,10 +1,26 @@
 package com.inspirelectronics.ld29;
 
 public class Game {
+	
+	public static final int WIDTH = 240;
+	public static final int HEIGHT = 160;
+	public static final String TITLE = "LD29";
 
+	public Game(){
+		
+	}
+	
+	public synchronized void start(){
+		
+	}
+	
+	public synchronized void stop(){
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Game game = new Game();
+		game.start();
 	}
 
 }
